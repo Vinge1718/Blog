db53f4829bce508f29699e9d9007aca94c017451
vinge1718 <vinge1718@gmail.com>
2019-11-08 12:52:21 +0300

    replace the author information with information from the database as well as the post body itself will be loaded from the database rather than static data

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -18,4 +18,10 @@ class BlogController extends Controller
                  ->simplePaginate($this->limit);
      return view("blog.index", compact('posts'));
    }
+
+ public function show($id)
+  {
+    $post = Post::findOrFail($id);
+    return view("blog.show", compact('post'));
+  }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,10 @@
 
 Route::get('/', [
     'uses' => 'BlogController@index',
-    'as' => 'blog'  
+    'as'   => 'blog'
 ]);
 
-Route::get('/blog/show', function () {
-    return view('blog.show');
-});
+Route::get('/blog/{post}', [
+    'uses' => 'BlogController@show',
+    'as'   => 'blog.show'
+]);
