b142d0bddf368eb071e1c16206cb362b4e3b8754
vinge1718 <vinge1718@gmail.com>
2020-04-27 22:26:26 +0300

    made the two fields belonging to the user table in the database that are not essential nullable seeing as the strict mode in MySQL was preventing the saving of the required details by requesting for a default value

--- database/migrations/2019_11_17_080127_alter_users_add_slug_column.php
+++ database/migrations/2019_11_17_080127_alter_users_add_slug_column.php
@@ -14,7 +14,7 @@ class AlterUsersAddSlugColumn extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('slug');
+            $table->string('slug')->nullable();
         });
     }
 
